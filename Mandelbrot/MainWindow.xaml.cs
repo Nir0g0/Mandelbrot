3941df7cfadfcd366e1e62b3b5895fafd0a5828c
Julien Kulbe <julien.kulbe@googlemail.com>
2021-04-17 21:18:10 +0200

    added control to modify draw objects

--- Mandelbrot/MainWindow.xaml.cs
+++ Mandelbrot/MainWindow.xaml.cs
@@ -63,127 +63,127 @@ namespace Mandelbrot {
 
         private void DrawRectangleClick(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                MakeScreenGreatAgain();
-                int widdth = 0;
-                int heigth = 0;
-                int radius = 0;
-                var wb = myImage.Source as WriteableBitmap;
-                if (Int32.TryParse(widthh.Text, out int result)) {
-                    widdth = Convert.ToInt32(widthh.Text);
-                } else {
-                    MessageBox.Show("Keine gültige Zahl");
-                }
-                if (Int32.TryParse(heigthh.Text, out int result2)) {
-                    heigth = Convert.ToInt32(heigthh.Text);
-                } else {
-                    MessageBox.Show("Keine gültige Zahl");
-                }
-                if (Int32.TryParse(widthh.Text, out int result3)) {
-                    radius = Convert.ToInt32(Radius.Text);
-                } else {
-                    MessageBox.Show("Keine gültige Zahl");
-                }
-                var rec = new Int32Rect(100, 100, widdth, heigth);
-                var stride = (rec.Width * PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
-                var bufferSize = rec.Height * stride;
-                byte[] pix = new byte[bufferSize];
-                for (int i = 0; i < widdth; i++) {
-                    for (int j = 0; j < heigth; j++)
-                    {
-                        var r = Math.Sqrt(Math.Pow(i - widdth / 2, 2) + Math.Pow(j - heigth / 2, 2));
-                        if (r <= radius)
-                        {
-                            pix[4 * (widdth * j + i) + 0] = 0; // b
-                            pix[4 * (widdth * j + i) + 1] = 0; // g
-                            pix[4 * (widdth * j + i) + 2] = 255; // r
-                            pix[4 * (widdth * j + i) + 3] = 255; // a
-                        }
-                        else
-                        {
-                            pix[4 * (widdth * j + i) + 0] = 255; // b
-                            pix[4 * (widdth * j + i) + 1] = 255; // g
-                            pix[4 * (widdth * j + i) + 2] = 255; // r
-                            pix[4 * (widdth * j + i) + 3] = 255; // a
-                        }
-                    }
-                }
-                wb.WritePixels(rec, pix, stride, 0);
+            //try
+            //{
+            //    MakeScreenGreatAgain();
+            //    int widdth = 0;
+            //    int heigth = 0;
+            //    int radius = 0;
+            //    var wb = myImage.Source as WriteableBitmap;
+            //    if (Int32.TryParse(widthh.Text, out int result)) {
+            //        widdth = Convert.ToInt32(widthh.Text);
+            //    } else {
+            //        MessageBox.Show("Keine gültige Zahl");
+            //    }
+            //    if (Int32.TryParse(heigthh.Text, out int result2)) {
+            //        heigth = Convert.ToInt32(heigthh.Text);
+            //    } else {
+            //        MessageBox.Show("Keine gültige Zahl");
+            //    }
+            //    if (Int32.TryParse(widthh.Text, out int result3)) {
+            //        radius = Convert.ToInt32(Radius.Text);
+            //    } else {
+            //        MessageBox.Show("Keine gültige Zahl");
+            //    }
+            //    var rec = new Int32Rect(100, 100, widdth, heigth);
+            //    var stride = (rec.Width * PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+            //    var bufferSize = rec.Height * stride;
+            //    byte[] pix = new byte[bufferSize];
+            //    for (int i = 0; i < widdth; i++) {
+            //        for (int j = 0; j < heigth; j++)
+            //        {
+            //            var r = Math.Sqrt(Math.Pow(i - widdth / 2, 2) + Math.Pow(j - heigth / 2, 2));
+            //            if (r <= radius)
+            //            {
+            //                pix[4 * (widdth * j + i) + 0] = 0; // b
+            //                pix[4 * (widdth * j + i) + 1] = 0; // g
+            //                pix[4 * (widdth * j + i) + 2] = 255; // r
+            //                pix[4 * (widdth * j + i) + 3] = 255; // a
+            //            }
+            //            else
+            //            {
+            //                pix[4 * (widdth * j + i) + 0] = 255; // b
+            //                pix[4 * (widdth * j + i) + 1] = 255; // g
+            //                pix[4 * (widdth * j + i) + 2] = 255; // r
+            //                pix[4 * (widdth * j + i) + 3] = 255; // a
+            //            }
+            //        }
+            //    }
+            //    wb.WritePixels(rec, pix, stride, 0);
 
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show(exception.ToString());
-            }
+            //}
+            //catch (Exception exception)
+            //{
+            //    MessageBox.Show(exception.ToString());
+            //}
         }
 
         private void DrawCircleClick(object sender, RoutedEventArgs e)
         {
             
-            var wb2 = myImage.Source as WriteableBitmap;
+            //var wb2 = myImage.Source as WriteableBitmap;
 
-            try {
-                MakeScreenGreatAgain();
-                int widdth = 0;
-                int heigth = 0;
-                var wb = myImage.Source as WriteableBitmap;
-                if (Int32.TryParse(widthh.Text, out int result)) {
-                    widdth = Convert.ToInt32(widthh.Text);
-                } else {
-                    MessageBox.Show("Keine gültige Zahl");
-                }
-                if (Int32.TryParse(heigthh.Text, out int result2)) {
-                    heigth = Convert.ToInt32(heigthh.Text);
-                } else {
-                    MessageBox.Show("Keine gültige Zahl");
-                }
-                var rec = new Int32Rect(100, 100, widdth, heigth);
-                var stride = (rec.Width * PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
-                var bufferSize = rec.Height * stride;
-                byte[] pix = new byte[bufferSize];
-                for (int i = 0; i < widdth * heigth; i++) {
-                    pix[4 * i + 0] = 0; // b
-                    pix[4 * i + 1] = 0; // g
-                    pix[4 * i + 2] = 0; // r
-                    pix[4 * i + 3] = 255; // a
-                }
-                wb.WritePixels(rec, pix, stride, 0);
-            } catch (Exception exception) {
-                MessageBox.Show(exception.ToString());
-            }
+            //try {
+            //    MakeScreenGreatAgain();
+            //    int widdth = 0;
+            //    int heigth = 0;
+            //    var wb = myImage.Source as WriteableBitmap;
+            //    if (Int32.TryParse(widthh.Text, out int result)) {
+            //        widdth = Convert.ToInt32(widthh.Text);
+            //    } else {
+            //        MessageBox.Show("Keine gültige Zahl");
+            //    }
+            //    if (Int32.TryParse(heigthh.Text, out int result2)) {
+            //        heigth = Convert.ToInt32(heigthh.Text);
+            //    } else {
+            //        MessageBox.Show("Keine gültige Zahl");
+            //    }
+            //    var rec = new Int32Rect(100, 100, widdth, heigth);
+            //    var stride = (rec.Width * PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+            //    var bufferSize = rec.Height * stride;
+            //    byte[] pix = new byte[bufferSize];
+            //    for (int i = 0; i < widdth * heigth; i++) {
+            //        pix[4 * i + 0] = 0; // b
+            //        pix[4 * i + 1] = 0; // g
+            //        pix[4 * i + 2] = 0; // r
+            //        pix[4 * i + 3] = 255; // a
+            //    }
+            //    wb.WritePixels(rec, pix, stride, 0);
+            //} catch (Exception exception) {
+            //    MessageBox.Show(exception.ToString());
+            //}
 
-            if (!Fill)
-            {
-                try {
-                    int widdth = 0;
-                    int heigth = 0;
-                    var wb = myImage.Source as WriteableBitmap;
-                    if (Int32.TryParse(widthh.Text, out int result)) {
-                        widdth = Convert.ToInt32(widthh.Text) - 2;
-                    } else {
-                        MessageBox.Show("Keine gültige Zahl");
-                    }
-                    if (Int32.TryParse(heigthh.Text, out int result2)) {
-                        heigth = Convert.ToInt32(heigthh.Text) - 2;
-                    } else {
-                        MessageBox.Show("Keine gültige Zahl");
-                    }
-                    var rec = new Int32Rect(100 + 1, 100 + 1, widdth, heigth);
-                    var stride = (rec.Width * PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
-                    var bufferSize = rec.Height * stride;
-                    byte[] pix = new byte[bufferSize];
-                    for (int i = 0; i < widdth * heigth; i++) {
-                        pix[4 * i + 0] = 255; // b
-                        pix[4 * i + 1] = 255; // g
-                        pix[4 * i + 2] = 255; // r
-                        pix[4 * i + 3] = 255; // a
-                    }
-                    wb.WritePixels(rec, pix, stride, 0);
-                } catch (Exception exception) {
-                    MessageBox.Show(exception.ToString());
-                }
-            }
+            //if (!Fill)
+            //{
+            //    try {
+            //        int widdth = 0;
+            //        int heigth = 0;
+            //        var wb = myImage.Source as WriteableBitmap;
+            //        if (Int32.TryParse(widthh.Text, out int result)) {
+            //            widdth = Convert.ToInt32(widthh.Text) - 2;
+            //        } else {
+            //            MessageBox.Show("Keine gültige Zahl");
+            //        }
+            //        if (Int32.TryParse(heigthh.Text, out int result2)) {
+            //            heigth = Convert.ToInt32(heigthh.Text) - 2;
+            //        } else {
+            //            MessageBox.Show("Keine gültige Zahl");
+            //        }
+            //        var rec = new Int32Rect(100 + 1, 100 + 1, widdth, heigth);
+            //        var stride = (rec.Width * PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+            //        var bufferSize = rec.Height * stride;
+            //        byte[] pix = new byte[bufferSize];
+            //        for (int i = 0; i < widdth * heigth; i++) {
+            //            pix[4 * i + 0] = 255; // b
+            //            pix[4 * i + 1] = 255; // g
+            //            pix[4 * i + 2] = 255; // r
+            //            pix[4 * i + 3] = 255; // a
+            //        }
+            //        wb.WritePixels(rec, pix, stride, 0);
+            //    } catch (Exception exception) {
+            //        MessageBox.Show(exception.ToString());
+            //    }
+            //}
         }
 
         private void Clear(object sender, RoutedEventArgs e)
